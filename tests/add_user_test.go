3e18980212692e440fb1407dc256cad72c789258
miguel-merlin <mmerlin@stevens.edu>
2023-11-04 01:35:30 -0400

    Added test cases in add_user_test

--- tests/add_user_test.go
+++ tests/add_user_test.go
@@ -117,3 +117,162 @@ users:
 		t.Errorf("Expected users.yaml data:\n%v\n\nBut got:\n%v", expectedDataObj, usersDataObj)
 	}
 }
+
+func TestAddUserHandler_UserAlreadyExists(t *testing.T) {
+	// Create a temporary users.yaml file for testing
+	tempFile, err := os.CreateTemp("", "users.yaml")
+	if err != nil {
+		t.Fatalf("Failed to create temporary file: %v", err)
+	}
+	defer tempFile.Close()
+
+	// Write initial data to the temporary file
+	initialData := `
+users:
+  existinguser:
+    disabled: false
+    displayname: Existing User
+    password: existingpassword
+    email: existinguser@example.com
+    groups:
+      - group1
+`
+	err = os.WriteFile(tempFile.Name(), []byte(initialData), 0644)
+	if err != nil {
+		t.Fatalf("Failed to write initial data to temporary file: %v", err)
+	}
+
+	// Create a test request JSON with an existing user
+	userReq := UserRequest{
+		Username:    "existinguser",
+		Password:    "newpassword",
+		Displayname: "New User",
+		Email:       "newuser@example.com",
+		Groups:      []string{"group1", "group2"},
+	}
+
+	jsonData, err := json.Marshal(userReq)
+	if err != nil {
+		t.Fatalf("Failed to marshal JSON: %v", err)
+	}
+
+	req, err := http.NewRequest("POST", "/adduser", bytes.NewBuffer(jsonData))
+	if err != nil {
+		t.Fatalf("Failed to create request: %v", err)
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	// Create a response recorder to capture the response
+	res := httptest.NewRecorder()
+
+	// Call the addUserHandler function with the test request and response recorder
+	handlers.AddUserHandler(res, req, tempFile.Name())
+
+	// Check the response status code
+	if res.Code != http.StatusOK {
+		t.Errorf("Expected status code %d, but got %d", http.StatusOK, res.Code)
+	}
+
+	// Read the updated users.yaml file
+	usersData, err := os.ReadFile(tempFile.Name())
+	if err != nil {
+		t.Fatalf("Failed to read users.yaml file: %v", err)
+	}
+
+	expectedData, err := readFile("test_users.yaml")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var usersDataObj, expectedDataObj interface{}
+	if err := yaml.Unmarshal(usersData, &usersDataObj); err != nil {
+		t.Fatal(err)
+	}
+	if err := yaml.Unmarshal(expectedData, &expectedDataObj); err != nil {
+		t.Fatal(err)
+	}
+
+	// Check the response status code
+	if res.Code != http.StatusBadRequest {
+		t.Errorf("Expected status code %d, but got %d", http.StatusBadRequest, res.Code)
+	}
+}
+
+func TestAddUserHandler_MissingFields(t *testing.T) {
+	tempFile, err := os.CreateTemp("", "users.yaml")
+	if err != nil {
+		t.Fatalf("Failed to create temporary file: %v", err)
+	}
+	defer tempFile.Close()
+
+	// Write initial data to the temporary file
+	initialData := `
+users:
+  existinguser:
+    disabled: false
+    displayname: Existing User
+    password: existingpassword
+    email: existinguser@example.com
+    groups:
+      - group1
+`
+	err = os.WriteFile(tempFile.Name(), []byte(initialData), 0644)
+	if err != nil {
+		t.Fatalf("Failed to write initial data to temporary file: %v", err)
+	}
+
+	// Create a test request JSON with missing fields
+	userReq := UserRequest{
+		Username: "newuser",
+		// Missing password
+		Displayname: "New User",
+		Email:       "newuser@example.com",
+		Groups:      []string{"group1", "group2"},
+	}
+
+	jsonData, err := json.Marshal(userReq)
+	if err != nil {
+		t.Fatalf("Failed to marshal JSON: %v", err)
+	}
+
+	req, err := http.NewRequest("POST", "/adduser", bytes.NewBuffer(jsonData))
+	if err != nil {
+		t.Fatalf("Failed to create request: %v", err)
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	// Create a response recorder to capture the response
+	res := httptest.NewRecorder()
+
+	// Call the addUserHandler function with the test request and response recorder
+	handlers.AddUserHandler(res, req, tempFile.Name())
+
+	// Check the response status code
+	if res.Code != http.StatusOK {
+		t.Errorf("Expected status code %d, but got %d", http.StatusOK, res.Code)
+	}
+
+	// Read the updated users.yaml file
+	usersData, err := os.ReadFile(tempFile.Name())
+	if err != nil {
+		t.Fatalf("Failed to read users.yaml file: %v", err)
+	}
+
+	expectedData, err := readFile("test_users.yaml")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var usersDataObj, expectedDataObj interface{}
+	if err := yaml.Unmarshal(usersData, &usersDataObj); err != nil {
+		t.Fatal(err)
+	}
+	if err := yaml.Unmarshal(expectedData, &expectedDataObj); err != nil {
+		t.Fatal(err)
+	}
+
+	// Check the response status code
+	if res.Code != http.StatusBadRequest {
+		t.Errorf("Expected status code %d, but got %d", http.StatusBadRequest, res.Code)
+	}
+}

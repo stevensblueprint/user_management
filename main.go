415416607e47cd22d3a5be23b96350254745f9a0
miguel-merlin <mmerlin@stevens.edu>
2024-01-22 20:15:08 -0500

    feat: Added endpoints in main.go

--- main.go
+++ main.go
@@ -10,11 +10,38 @@ import (
 
 var PORT = ":3000"
 var PATH = "etc/users.yaml"
+var BASE_URL = "v1/users"
 
 func main() {
 	// Set up the routes
-	http.HandleFunc("/add-user", func(w http.ResponseWriter, r *http.Request) {
-		handlers.AddUserHandler(w, r, PATH)
+	http.HandleFunc(BASE_URL+"/user", func(w http.ResponseWriter, r *http.Request) {
+		// POST /v1/users/user
+		if r.Method == http.MethodPost {
+			handlers.AddUserHandler(w, r, PATH)
+		}
+
+		// Return 404 for all other methods
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	})
+
+	http.HandleFunc(BASE_URL+"/user/disable", func(w http.ResponseWriter, r *http.Request) {
+		// POST /v1/users/user/disable
+		if r.Method == http.MethodPost {
+			handlers.DisableUserHandler(w, r, PATH)
+		}
+
+		// Return 404 for all other methods
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	})
+
+	http.HandleFunc(BASE_URL+"/all", func(w http.ResponseWriter, r *http.Request) {
+		// GET /v1/users/all
+		if r.Method == http.MethodGet {
+			handlers.GetAllUsersHandler(w, r, PATH)
+		}
+
+		// Return 404 for all other methods
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 	})
 
 	// Health check

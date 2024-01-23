8c65352ae1e9bfc5345c44f0495b8fd878023ed9
miguel-merlin <mmerlin@stevens.edu>
2024-01-22 19:32:53 -0500

    feat: added disable user handler

---
+++ handlers/disable_user_handler.go
@@ -0,0 +1,66 @@
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+	"os"
+	"user_management/models"
+
+	"gopkg.in/yaml.v2"
+)
+
+type DisableUserRequest struct {
+	Username string `json:"username"`
+}
+
+func DisableUserHandler(w http.ResponseWriter, r *http.Request, filePath string) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var userReq DisableUserRequest
+	err := json.NewDecoder(r.Body).Decode(&userReq)
+	if err != nil {
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
+		return
+	}
+
+	// Read the existing users.yaml file
+	usersData, err := os.ReadFile(filePath)
+	if err != nil {
+		http.Error(w, "Failed to open users.yaml file", http.StatusInternalServerError)
+		return
+	}
+
+	var users models.Users
+	err = yaml.Unmarshal(usersData, &users)
+	if err != nil {
+		http.Error(w, "Failed to parse users.yaml file", http.StatusInternalServerError)
+		return
+	}
+
+	// Check if the username exists
+	if _, exists := users.Users[userReq.Username]; !exists {
+		http.Error(w, "Username does not exist", http.StatusBadRequest)
+		return
+	}
+
+	// Disable the user
+	user := users.Users[userReq.Username]
+	user.Disabled = true
+	users.Users[userReq.Username] = user
+
+	// Write the updated users.yaml file
+	usersData, err = yaml.Marshal(users)
+	if err != nil {
+		http.Error(w, "Failed to convert users to YAML", http.StatusInternalServerError)
+		return
+	}
+
+	err = os.WriteFile(filePath, usersData, 0644)
+	if err != nil {
+		http.Error(w, "Failed to write users.yaml file", http.StatusInternalServerError)
+		return
+	}
+}

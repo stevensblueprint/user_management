8cf6c2429dbb6ec4c23207ff0c00f385a14905ba
miguel-merlin <mmerlin@stevens.edu>
2024-02-01 00:24:09 -0500

    tests: Added disable user handler success

---
+++ tests/disable_user_handler.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"user_management/handlers"
+)
+
+func TestDisableUserHandlerSuccess(t *testing.T) {
+	// Reset test_users.yaml file
+	err := resetYAMLFile("test_users.yaml")
+	if err != nil {
+		t.Fatalf("Failed to reset YAML file: %v", err)
+	}
+
+	// Setup request body
+	disableReq := handlers.DisableUserRequest{
+		Username: "existinguser",
+	}
+
+	// Convert Request body to JSON``
+	body, _ := json.Marshal(disableReq)
+
+	// Create an HTTP request
+	req, err := http.NewRequest("POST", "/disableuser", bytes.NewBuffer(body))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		handlers.DisableUserHandler(w, r, "test_users.yaml")
+	})
+
+	// Call the handler
+	handler.ServeHTTP(rr, req)
+
+	// Check status code and response
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
+	}
+
+	expected := "User disabled successfully"
+	if rr.Body.String() != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v", rr.Body.String(), expected)
+	}
+
+}
